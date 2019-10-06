c91d9d0bde4ad61a7be870286f08d0c75a819071
Dirk Eddelbuettel <edd@debian.org>
2019-10-06 07:44:26 -0500

    update data helper and data file, create manual page for data

--- R/dataHelper.R
+++ R/dataHelper.R
@@ -1,8 +1,24 @@
 
 ## use PINs in the pin/river-forest* files to obtain data set
 .getData <- function() {
-    setwd(system.file("data", package="cook-county-tax-model")
-    csvfiles <- dir("pin/", pattern="river-forest*.csv", full.name=TRUE)
+    setwd(system.file("extdata", package="ccptm"))
+    csvfiles <- dir("pin/", pattern="river-forest*.csv", full.names=TRUE)
     data <- files2data(csvfiles)
-    fwrite(data, "riverforest.csv")
+    fwrite(data, system.file("data", "riverforest.csv", package="ccptm"))
 }
+
+#' River Forest, IL, Property Tax Data
+#'
+#' Data has been scraped downloaded from the Cook County, IL, property tax website
+#' at <https://www.cookcountyassessor.com/Search/Property-Owner-Search.aspx> using
+#' combination of a) Property Index Number (PIN) search via the URL
+#' \url{http://www.cookcountyassessor.com/Search/Property-Search.aspx} and
+#' b) per-prortery access given a pin (as final argument) via
+#' \url{http://www.cookcountyassessor.com/Property.aspx?mode=details&pin=...}.
+#' The resulting data set contains all columns provided by the website.
+#' @name riverforest
+#' @docType data
+#' @usage data(riverforest)
+#' @format An data.frame (or data.table) object
+#' @keywords datasets
+NULL
